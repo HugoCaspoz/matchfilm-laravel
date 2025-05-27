55d22489f2636d7a0ad31d3a53195651c3d17aa2
hcasado <hcasado@titaniumindustrialsecurity.com>
2025-05-27 15:05:55 +0200

    push

--- resources/views/matches/index.blade.php
+++ resources/views/matches/index.blade.php
@@ -33,9 +33,6 @@
                             <i class="fas fa-users text-4xl text-gray-400 mb-4"></i>
                             <h3 class="text-xl font-semibold mb-2">Aún no tienes amigos</h3>
                             <p class="text-gray-500 mb-4">Para ver matches, primero debes agregar amigos.</p>
-                            <a href="{{ route('friends.index') }}" class="inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-600 focus:bg-red-600 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                Agregar Amigos
-                            </a>
                         </div>
                     @else
                         <div class="friend-selector">
